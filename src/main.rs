dace3c9c0c53d6ab980153b8cb9e954e2c297238
daikw <hazuhi@gmail.com>
2021-01-09 22:17:35 +0900

    :+1: hello, primitives

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 mod guess_the_number;
 mod primitives;
 
-pub use crate::guess_the_number::dialogue;
+// pub use crate::guess_the_number::dialogue;
 pub use crate::primitives::functions;
+pub use crate::primitives::variables;
 
 fn main() {
-  println!("Hello, world!");
-
-  functions::puts();
-
-  dialogue::main();
+  // functions::puts();
+  // dialogue::main();
+  variables::example();
 }
--- src/primitives.rs
+++ src/primitives.rs
@@ -3,3 +3,32 @@ pub mod functions {
     println!("called primitives function");
   }
 }
+
+pub mod variables {
+  pub fn example() {
+    println!("this is valid:");
+    println!(
+      "\n\
+      ```\n\
+      let spaces = \"    \";\n\
+      let spaces = spaces.len();\n\
+      ```\n\
+      "
+    );
+
+    let spaces = "    ";
+    let spaces = spaces.len();
+
+    println!("result: {}\n", spaces);
+
+    println!("but this is invalid:");
+    println!(
+      "\n\
+      ```\n\
+      let mut spaces = \"    \";\n\
+      spaces = spaces.len();\n\
+      ```\n\
+      "
+    );
+  }
+}
