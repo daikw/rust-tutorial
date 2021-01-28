0c5a2c42b49322a029ae4f5f2245b6d539058dbc
daikw <hazuhi@gmail.com>
2021-01-28 09:07:04 +0900

    :recycle: attempt to split into modules

--- src/draw_functions.rs
+++ src/draw_functions.rs
@@ -1,116 +1,120 @@
-type Function = fn(f64, f64) -> f64;
-type F = Function; // alias
+pub mod functions {
+  extern crate libm;
 
-pub fn f(x: f64, y: f64) -> f64 {
-  return x + y;
-}
+  pub type Function = fn(f64, f64) -> f64;
+  pub type F = Function; // alias
 
-extern crate libm;
-pub fn sin_rr(x: f64, y: f64) -> f64 {
-  let r = libm::hypot(x, y);
-  return r.sin() / r;
-}
-
-pub struct Canvas {
-  width: i64,
-  height: i64,
-  xyrange: f64,
-  cells: i64,
-}
-
-impl Canvas {
-  pub fn default() -> Canvas {
-    return Canvas {
-      width: 600,
-      height: 320,
-      xyrange: 30.0,
-      cells: 100,
-    };
+  pub fn sample() -> F {
+    return |x: f64, y: f64| -> f64 { return x * y };
   }
 
-  pub fn new(width: i64, height: i64, xyrange: f64, cells: i64) -> Canvas {
-    return Canvas {
-      width: width,
-      height: height,
-      xyrange: xyrange,
-      cells: cells,
-    };
+  pub fn sum(x: f64, y: f64) -> f64 {
+    return x + y;
   }
 
-  pub fn project(&self, f: F, i: i64, j: i64) -> [f64; 2] {
-    let x = self.xyrange * ((i as f64) / (self.cells as f64) - 0.5);
-    let y = self.xyrange * ((j as f64) / (self.cells as f64) - 0.5);
-    let z = f(x, y);
+  pub fn sin_rr(x: f64, y: f64) -> f64 {
+    let r = libm::hypot(x, y);
+    return r.sin() / r;
+  }
 
-    let angle = std::f64::consts::FRAC_PI_6;
-    let xyscale = (self.width as f64) / 2.0 / self.xyrange;
-    let zscale = (self.height as f64) * 0.4;
-
-    let sx = (self.width as f64) / 2.0 + (x - y) * angle.cos() * xyscale;
-    let sy = (self.height as f64) / 2.0 + (x + y) * angle.sin() * xyscale - z * zscale;
-
-    return [sx, sy];
+  #[test]
+  fn test_sum() {
+    assert_eq!(sum(2.0, 2.0), 4.0);
   }
 }
 
-struct SVGRenderer {
-  f: Function,
-  canvas: Canvas,
-}
+pub mod renderers {
+  use crate::draw_functions::functions;
 
-use std::fs::File;
-use std::io::{BufWriter, Write};
-use std::path::Path;
-
-impl SVGRenderer {
-  pub fn new(function: F, canvas: Canvas) -> SVGRenderer {
-    return SVGRenderer {
-      f: function,
-      canvas: canvas,
-    };
+  pub struct Canvas {
+    width: i64,
+    height: i64,
+    xyrange: f64,
+    cells: i64,
   }
 
-  fn write(&self, path_string: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let path = Path::new(path_string);
-    let file = File::create(path)?;
-    let mut writer = BufWriter::new(file);
-
-    let header = format!(
-      "<?xml version=\"1.0\"?><svg xmlns=\"http://www.w3.org/2000/svg\"
-         style='stroke: grey; fill: white; stroke-width: 0.7' width='{}' height='{}'>\n",
-      self.canvas.width, self.canvas.height
-    );
-    writer.write(header.as_bytes())?;
-
-    for i in 0..self.canvas.cells {
-      for j in 0..self.canvas.cells {
-        let [ax, ay] = self.canvas.project(self.f, i + 1, j);
-        let [bx, by] = self.canvas.project(self.f, i, j);
-        let [cx, cy] = self.canvas.project(self.f, i, j + 1);
-        let [dx, dy] = self.canvas.project(self.f, i + 1, j + 1);
-
-        let polygon = format!(
-          "  <polygon points='{},{} {},{} {},{} {},{}'/>\n",
-          ax, ay, bx, by, cx, cy, dx, dy
-        );
-
-        writer.write(polygon.as_bytes())?;
-      }
+  impl Canvas {
+    pub fn default() -> Canvas {
+      return Canvas {
+        width: 600,
+        height: 320,
+        xyrange: 30.0,
+        cells: 100,
+      };
     }
+    pub fn new(width: i64, height: i64, xyrange: f64, cells: i64) -> Canvas {
+      return Canvas {
+        width: width,
+        height: height,
+        xyrange: xyrange,
+        cells: cells,
+      };
+    }
+    fn project(&self, f: functions::Function, i: i64, j: i64) -> [f64; 2] {
+      let x = self.xyrange * ((i as f64) / (self.cells as f64) - 0.5);
+      let y = self.xyrange * ((j as f64) / (self.cells as f64) - 0.5);
+      let z = f(x, y);
+      let angle = std::f64::consts::FRAC_PI_6;
+      let xyscale = (self.width as f64) / 2.0 / self.xyrange;
+      let zscale = (self.height as f64) * 0.4;
+      let sx = (self.width as f64) / 2.0 + (x - y) * angle.cos() * xyscale;
+      let sy = (self.height as f64) / 2.0 + (x + y) * angle.sin() * xyscale - z * zscale;
+      return [sx, sy];
+    }
+  }
 
-    let footer = "</svg>";
-    writer.write(footer.as_bytes())?;
+  use std::fs::File;
+  use std::io::{BufWriter, Write};
+  use std::path::Path;
 
+  struct SVGRenderer {
+    f: functions::Function,
+    canvas: Canvas,
+  }
+
+  impl SVGRenderer {
+    pub fn new(function: functions::Function, canvas: Canvas) -> SVGRenderer {
+      return SVGRenderer {
+        f: function,
+        canvas: canvas,
+      };
+    }
+    fn write(&self, path_string: &str) -> Result<(), Box<dyn std::error::Error>> {
+      let path = Path::new(path_string);
+      let file = File::create(path)?;
+      let mut writer = BufWriter::new(file);
+      let header = format!(
+        "<?xml version=\"1.0\"?><svg xmlns=\"http://www.w3.org/2000/svg\"
+           style='stroke: grey; fill: white; stroke-width: 0.7' width='{}' height='{}'>\n",
+        self.canvas.width, self.canvas.height
+      );
+      writer.write(header.as_bytes())?;
+      for i in 0..self.canvas.cells {
+        for j in 0..self.canvas.cells {
+          let [ax, ay] = self.canvas.project(self.f, i + 1, j);
+          let [bx, by] = self.canvas.project(self.f, i, j);
+          let [cx, cy] = self.canvas.project(self.f, i, j + 1);
+          let [dx, dy] = self.canvas.project(self.f, i + 1, j + 1);
+          let polygon = format!(
+            "  <polygon points='{},{} {},{} {},{} {},{}'/>\n",
+            ax, ay, bx, by, cx, cy, dx, dy
+          );
+          writer.write(polygon.as_bytes())?;
+        }
+      }
+      let footer = "</svg>";
+      writer.write(footer.as_bytes())?;
+      Ok(())
+    }
+  }
+
+  pub fn draw(
+    path_string: &str,
+    f: functions::Function,
+    canvas: Canvas,
+  ) -> Result<(), Box<dyn std::error::Error>> {
+    let renderer = SVGRenderer::new(f, canvas);
+    renderer.write(path_string)?;
     Ok(())
   }
-}
-
-pub fn draw(
-  path_string: &str,
-  f: Function,
-  canvas: Canvas,
-) -> Result<(), Box<dyn std::error::Error>> {
-  let renderer = SVGRenderer::new(f, canvas);
-  renderer.write(path_string)?;
-  Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,14 @@
 mod draw_functions;
-mod guess_the_number;
-mod primitives;
 
-pub use crate::guess_the_number::dialogue;
-pub use crate::primitives::functions;
-pub use crate::primitives::variables;
+pub use crate::draw_functions::functions as funcs;
+pub use crate::draw_functions::renderers as render;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-  // fn main() {
-  // functions::puts();
-  // dialogue::main();
-  // variables::example();
-
   let path = "image.svg";
+  let function = funcs::sin_rr;
+  let canvas = render::Canvas::default();
 
-  // let function = draw_functions::f;
-  let function = draw_functions::sin_rr;
-  // let function = |x: f64, y: f64| -> f64 { return x * y };
-
-  let canvas = draw_functions::Canvas::default();
-
-  draw_functions::draw(path, function, canvas)?;
+  render::draw(path, function, canvas)?;
 
   Ok(())
 }
