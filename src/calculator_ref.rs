7fa607212a00142d6aa1cb6c4acb15c397444d34
daikw <hazuhi@gmail.com>
2021-02-18 23:03:37 +0900

    :recycle:

--- src/calculator_ref.rs
+++ src/calculator_ref.rs
@@ -1,7 +1,6 @@
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 struct Loc(usize, usize);
 impl Loc {
-  #[allow(dead_code)]
   fn merge(&self, other: &Loc) -> Loc {
     use std::cmp::{max, min};
     Loc(min(self.0, other.0), max(self.1, other.1))
